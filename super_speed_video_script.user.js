2846ca81afd440a6dfc1e7d1f41581f3f0825a83
wll <1984643280@qq.com>
2023-12-04 19:32:28 +0800

    readme

--- super_speed_video_script.user.js
+++ super_speed_video_script.user.js
@@ -11,6 +11,8 @@
 // @run-at       document-end
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_openInTab
 // @license      GPL-3.0
 // @downloadURL  https://update.greasyfork.org/scripts/480875/%E8%B6%85%E7%BA%A7%E8%A7%86%E9%A2%91%E5%80%8D%E9%80%9F%E6%92%AD%E6%94%BE.user.js
 // @updateURL    https://update.greasyfork.org/scripts/480875/%E8%B6%85%E7%BA%A7%E8%A7%86%E9%A2%91%E5%80%8D%E9%80%9F%E6%92%AD%E6%94%BE.meta.js
